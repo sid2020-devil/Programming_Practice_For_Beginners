ec0bdfb10c709c3478bae49a0b200849e678387e
A Krishnaprasad <krishna.arakkal@gmail.com>
2021-10-14 21:57:42 +0530

    pattern.sh
    shell script to display a triangle pattern

---
+++ Shell/pattern.sh
@@ -0,0 +1,25 @@
+read num
+
+for((i=1;i<=num;i++))
+
+	do
+
+	for((k=1;k<=num-i;k++))
+
+		do
+	
+			echo -n " "
+
+		done
+
+	for((j=1;j<=2\*i-1;j++))
+
+		do
+
+			echo -n "*"
+
+		done
+
+	echo " "
+
+done
