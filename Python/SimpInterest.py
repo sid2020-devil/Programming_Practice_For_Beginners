3ab6c4c5eb76449db5bc96010ac8897ead917129
Anurag N Nair <78994285+anuragnnair007@users.noreply.github.com>
2021-10-17 19:45:54 +0530

    Create SimpInterest.py

---
+++ Python/SimpInterest.py
@@ -0,0 +1,7 @@
+p=int(input("enter the principle amount"));
+i=float(input("enter the basic interest"));
+t=int(input("enter the time period for loan"));
+si=p*t*i/100;
+print("The Simple Interest for the Principle Amount %d at an interest rate of %f for the time period %d is : %f"%(p,i,t,si));
+
+#Contributed by Anurag
